7ea54e6ef73847013348cb61c0e565557d0f3b17
Nehagarde <neha.r.garde@gmail.com>
2019-07-06 14:44:18 +0530

    Committing ETL classes directory

--- files.py
+++ files.py
@@ -4,6 +4,23 @@ def write_to_file():
         l=["neha \n ","garde"]
         f.writelines(l)
 
+def read_from_file():
+    with open("demo.txt","r") as f:
+        #f.read()
+        for line in f:
+            print(line)
+            #break
+
+        l=f.readlines()
+
+def append_file():
+    with open("demo.txt","a+") as f:
+        f.write("\n Hello Neha. How r u doing today?")
+        f.seek(0)
+        print(f.read())
+
 
 if __name__ == '__main__':
     write_to_file()
+    append_file()
+    #read_from_file()
